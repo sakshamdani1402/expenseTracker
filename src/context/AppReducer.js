fb7ab89f27ab2ff1b0c41322b3c57e0e1e99c4c4
sakshamdani1402 <sakshamdsni@gmail.com>
2022-02-17 00:20:43 +0530

    day 2 - minor bugs

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+function reducer(state, action){
     switch (action.type) {
         case 'DELETE_TRANSACTION':
             return {
@@ -16,3 +16,4 @@ export default (state, action) => {
             return state;
     }
 }
+export default reducer
